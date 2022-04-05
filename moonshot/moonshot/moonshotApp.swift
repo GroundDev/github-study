377a8974c133836f244115a621d5e281111bf5f9
asher <dud3576@naver.com>
2022-04-05 12:55:28 +0900

    xcode 파일 추가

---
+++ moonshot/moonshot/moonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  moonshotApp.swift
+//  moonshot
+//
+//  Created by 임영후 on 2022/04/05.
+//
+
+import SwiftUI
+
+@main
+struct moonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
